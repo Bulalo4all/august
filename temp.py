de256085a23dd2f75f8ea0729600f6c0ddec8035
Bulalo4all <147882966+Bulalo4all@users.noreply.github.com>
2023-10-16 15:32:01 -0600

    Update temp.py
    Step 2: Create function for C to F

--- temp.py
+++ temp.py
@@ -2,3 +2,9 @@ x = (25 * (9/5)) + 32
 x = float(x)
 # formula for Celsius to Farenheit = (Temp in Celsius * 9/5) + 32
 print(f"25 Degrees in Celsius is {x:.1f} in farenheit")
+
+# Step 2:
+def celsius_to_farenheit(celsius):
+    farenheit = (celsius * 9/5) + 32
+    print(f"{celsius} degrees in celsius is {farenheit:.1f} in farenheit")
+celsius_to_farenheit(25)
