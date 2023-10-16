363d21aa207bf6abd4a7fbb8bb1bae872f740cb5
Bulalo4all <147882966+Bulalo4all@users.noreply.github.com>
2023-10-16 15:32:31 -0600

    Update temp.py
    Step #3: Convert F to C

--- temp.py
+++ temp.py
@@ -8,3 +8,9 @@ def celsius_to_farenheit(celsius):
     farenheit = (celsius * 9/5) + 32
     print(f"{celsius} degrees in celsius is {farenheit:.1f} in farenheit")
 celsius_to_farenheit(25)
+
+#Step 3:
+def farenheit_to_celsius(farenheit):
+    celsius = (farenheit - 32) * (5/9)
+    print(f"{farenheit} degreeds in farenheit is {celsius:.1f} in celsius")
+farenheit_to_celsius(82)
